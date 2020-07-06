38e732bb428502b2692e630163dd9fb483a13a13
Julien Ballanger <ballanger.julien@gmail.com>
2020-07-06 16:26:31 +0200

    Add microphone level check helper

---
+++ src/helpers/microphoneLevelCheck.tsx
@@ -0,0 +1,53 @@
+interface MicrophoneCheckOptions {
+  onMicrophoneUp?: () => void;
+  onMicrophoneDown?: () => void;
+}
+
+let interval: number | null = null;
+let checkingSince = 0;
+let microphoneLevelSum = 0;
+let numberOfFailedCheckInARow = 0;
+let lastState = 'up';
+
+const onMicrophoneLevel = (event: CustomEvent) => {
+  const { value: microphoneLevel } = event.detail.data;
+  microphoneLevelSum += microphoneLevel;
+};
+
+export const endMicrophoneLevelCheck = () => {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+  }
+  window.removeEventListener('snapcallEvent_audioLevelLocalMicrophone', onMicrophoneLevel as EventListener);
+  checkingSince = 0;
+  microphoneLevelSum = 0;
+  numberOfFailedCheckInARow = 0;
+};
+
+const microphoneCheck = ({ onMicrophoneDown, onMicrophoneUp }: MicrophoneCheckOptions) => {
+  if (checkingSince >= 10 && lastState === 'up') {
+    endMicrophoneLevelCheck();
+    return;
+  }
+  if (microphoneLevelSum < 1) {
+    numberOfFailedCheckInARow += 1;
+    if (numberOfFailedCheckInARow >= 5) {
+      lastState = 'down';
+      numberOfFailedCheckInARow = 0;
+      if (onMicrophoneDown) onMicrophoneDown();
+    }
+  } else {
+    if (onMicrophoneUp) onMicrophoneUp();
+    numberOfFailedCheckInARow = 0;
+    lastState = 'up';
+  }
+  microphoneLevelSum = 0;
+  checkingSince += 1;
+};
+
+export const checkMicrophoneLevel = (options: MicrophoneCheckOptions) => {
+  window.addEventListener('snapcallEvent_audioLevelLocalMicrophone', onMicrophoneLevel as EventListener);
+  const bindedCheck = microphoneCheck.bind(null, options);
+  interval = setInterval(bindedCheck, 1000);
+};
