784b3e2fd52950f03e0e771d93f2e2a46205a673
jballanger <ballanger.julien@gmail.com>
2021-09-28 12:33:22 +0200

    Optimize SVG icons size

--- src/components/video/icons/Camera.tsx
+++ src/components/video/icons/Camera.tsx
@@ -1,75 +1,23 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-const CameraIcon = ({ slash }: { slash: boolean }) => {
-  if (slash) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-      >
-        <g
-          id="Groupe_2251"
-          data-name="Groupe 2251"
-          transform="translate(-141 -107)"
-        >
-          <rect
-            id="Rectangle_1005"
-            data-name="Rectangle 1005"
-            width="20"
-            height="20"
-            transform="translate(141 107)"
-            fill="none"
-          />
-          <path
-            id="Camera"
-            d="M2.188,11.792A2.2,2.2,0,0,1,0,9.6V2.2A2.2,2.2,0,0,1,2.188,0h9.124A2.184,2.184,0,0,1,13.5,2.2V9.6a2.2,2.2,0,0,1-2.187,2.2Zm15.839-1.381L14.6,8.433V3.338l3.413-1.976a1.119,1.119,0,0,1,.372-.154A1.319,1.319,0,0,1,20,2.481V9.266a1.344,1.344,0,0,1-.175.659,1.315,1.315,0,0,1-1.136.665,1.3,1.3,0,0,1-.66-.181Z"
-            transform="translate(141 111.104)"
-            fill="#fff"
-          />
-          <path
-            id="Tracé_247"
-            data-name="Tracé 247"
-            d="M0,0H26.958V1.326H0Z"
-            transform="translate(141.938 107) rotate(45)"
-            fill="#d84949"
-          />
-        </g>
-      </svg>
-    );
-  }
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-    >
-      <g
-        id="Groupe_2250"
-        data-name="Groupe 2250"
-        transform="translate(-115 -107)"
-      >
-        <rect
-          id="Rectangle_1005"
-          data-name="Rectangle 1005"
-          width="20"
-          height="20"
-          transform="translate(115 107)"
-          fill="none"
-        />
-        <path
-          id="Camera"
-          d="M2.188,11.792A2.2,2.2,0,0,1,0,9.6V2.2A2.2,2.2,0,0,1,2.188,0h9.124A2.184,2.184,0,0,1,13.5,2.2V9.6a2.2,2.2,0,0,1-2.187,2.2Zm15.839-1.381L14.6,8.433V3.338l3.413-1.976a1.119,1.119,0,0,1,.372-.154A1.319,1.319,0,0,1,20,2.481V9.266a1.344,1.344,0,0,1-.175.659,1.315,1.315,0,0,1-1.136.665,1.3,1.3,0,0,1-.66-.181Z"
-          transform="translate(115 111.104)"
-          fill="#fff"
-        />
-      </g>
-    </svg>
-  );
-};
+const CameraIcon = ({ slash }: { slash: boolean }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+  >
+    <g>
+      <path fill="none" d="M0 0h20v20H0z" />
+      <path
+        d="M2.188 15.896A2.2 2.2 0 0 1 0 13.704v-7.4a2.2 2.2 0 0 1 2.188-2.2h9.124a2.184 2.184 0 0 1 2.188 2.2v7.4a2.2 2.2 0 0 1-2.187 2.2Zm15.839-1.381L14.6 12.537V7.442l3.413-1.976a1.119 1.119 0 0 1 .372-.154A1.319 1.319 0 0 1 20 6.585v6.785a1.344 1.344 0 0 1-.175.659 1.315 1.315 0 0 1-1.136.665 1.3 1.3 0 0 1-.66-.181Z"
+        fill="#fff"
+      />
+      {slash && <path d="M.938 0 20 19.062l-.937.938L0 .938Z" fill="#d84949" />}
+    </g>
+  </svg>
+);
 
 CameraIcon.defaultProps = {
   slash: false,
